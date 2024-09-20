71219488769b74b2fdc9ada81544b661af1264b1
Benjamín Serrano <benser22@hotmail.com>
2024-09-19 21:42:58 -0300

    ads removed

--- index.js
+++ index.js
@@ -1,56 +1,46 @@
-const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const axios = require("axios");
-const cheerio = require("cheerio");
-const cors = require("cors");
-let dotenv = require("dotenv").config();
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cors = require('cors');
+let dotenv = require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.options("*", cors());
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
+app.options('*', cors());
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
-app.use("/", express.static(__dirname + "/views/index.html"));
-app.get("/", async (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
-
-app.use("/news", express.static(__dirname + "/views/news.html"));
-app.get("/news", async (req, res) => {
-  const datos = {
-    variable1: "valor1",
-    variable2: "valor2",
-  };
-  res.render(__dirname + "/views/news.html", { datos });
+// Sirve el archivo estático index.html en la ruta '/home'
+app.use('/home', express.static(__dirname + '/views/index.html'));
+app.get('/home', async (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 //-----------------------------------------------------------------------
-const apiProxy1 = createProxyMiddleware("/", {
-  target: process.env.WEB_URL,
-  changeOrigin: true,
-});
-
-const apiProxy2 = createProxyMiddleware("/2", {
-  target: process.env.WEB_URL,
-  changeOrigin: true,
-});
-
-app.get("/noadd", async (req, res) => {
+// elimina el div del anuncio
+app.get('/', async (req, res) => {
   try {
-    const response = await axios.get(`http://localhost:4001/business/`);
+    const response = await axios.get(process.env.WEB_URL);
     const $ = cheerio.load(response.data);
-    $(".leaderboard__container__2MbZl").remove(); // Eliminar todos los elementos con la clase 'clase-a-eliminar' del DOM
+
+    // Eliminar el div con la clase 'ad ad-giga ad-giga-1'
+    $('.ad.ad-giga.ad-giga-1').remove();
+
     res.send($.html());
   } catch (error) {
     res.send(`Error: ${error.message}`);
   }
 });
 
+// Proxy para la URL raíz
+const apiProxy1 = createProxyMiddleware('/', {
+  target: process.env.WEB_URL,
+  changeOrigin: true,
+});
 app.use(apiProxy1);
-app.use(apiProxy2);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
